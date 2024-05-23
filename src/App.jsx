670b928b85c794d7b29a705463e5668fabf3c9c9
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-23 11:25:33 +0800

    storing cart data in firebase realtime db

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
 function App() {
   const isCartShow = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    console.log("cart updated");
+    fetch(
+      `https://adv-redux-2fe94-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   return (
     <>
       <Layout>
